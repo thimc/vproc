a4ba00ccf310784c16013fbfac524967abd6334a
Thim Cederlund <thim@cederlund.de>
2023-12-26 14:16:36 +0100

    style: tidy up the code and drop unused functions etc

--- vproc.c
+++ vproc.c
@@ -7,8 +7,7 @@
 #include <mouse.h>
 
 typedef struct Process Process;
-struct Process
-{
+struct Process {
 	char user[1024];
 	char cmd[1024];
 	char args[2048];
@@ -17,31 +16,28 @@ struct Process
 	long u, s;
 	int pid, siz;
 };
-Process* proclist;
+Process *proclist;
 
-enum
-{
+enum {
 	Emouse,
 	Ekeyboard,
 	Eresize,
 	Etimer,
 };
 
-enum
-{
-	Scrollwidth = 13,
+enum {
+	Scrollwidth = 13,	/* scrollbar width */
+	Vstep = 30,		/* vertical distance between cells */
+	Hstep = 70,		/* minimum horizontal distance between cells */
+	Theight = 25,		/* toolbar height */
+	Thoffset = 15,		/* horziontal text offset */
 };
 
-enum
-{
-	Vstep = 30, /* vertical distance between cells */
-	Hstep = 70, /* minimum horizontal distance between cells */
-	Theight = 25, /* toolbar height */
-	Thoffset = 15, /* horziontal text offset */
+char *headers[] = { 
+	"pid", "user", "utime", "stime", "rtime",
+	"size", "state", "command", "command + args", nil
 };
 
-char* headers[] = { "pid", "user", "utime", "stime", "rtime",
-			"size", "state", "command", "command + args", nil };
 int nprocs;
 int visprocs;
 int delay;
@@ -53,14 +49,14 @@ int isscrolling;
 int oldbuttons;
 int hstep;
 
-Image* toolbg;
-Image* toolfg;
-Image* viewbg;
-Image* viewfg;
-Image* selbg;
-Image* selfg;
-Image* scrollbg;
-Image* scrollfg;
+Image *toolbg;
+Image *toolfg;
+Image *viewbg;
+Image *viewfg;
+Image *selbg;
+Image *selfg;
+Image *scrollbg;
+Image *scrollfg;
 Rectangle toolr;
 Rectangle viewr;
 Rectangle scrollr;
@@ -71,13 +67,13 @@ Mousectl *mctl;
 Biobuf *b;
 
 int cmp(void *va, void *vb);
-void emouse(Mouse* m);
+void emouse(Mouse *m);
 void ekeyboard(Rune k);
 void eresize(void);
 int scroll(int d);
 void timerproc(void *c);
 void redraw(void);
-int drawprocfield(Image* screen, int px, int py, char* fmt, ...);
+void drawprocfield(int *px, int py, char *fmt, ...);
 void loaddir(void);
 void loadtheme(void);
 void usage(void);
@@ -87,23 +83,23 @@ getrowcount(void)
 {
 	int i, c;
 
-	for(i=0,c=0; headers[i] != nil; i++)
-		c++;
-	return c+1;
+	for(i=0, c=1; headers[i] != nil; i++, c++)
+		;
+	return c;
 }
 
 int
 cmp(void *va, void *vb)
 {
-	Process* a = (Process*)va;
-	Process* b = (Process*)vb;
+	Process *a = (Process*)va;
+	Process *b = (Process*)vb;
 	if(reverseoflag)
 		return b->pid - a->pid;
 	return a->pid - b->pid;
 }
 
 void
-emouse(Mouse* m)
+emouse(Mouse *m)
 {
 	if(oldbuttons==0 && m->buttons!=0 && ptinrect(m->xy, scrollr))
 		isscrolling = 1;
@@ -167,15 +163,16 @@ ekeyboard(Rune k)
 void
 eresize(void)
 {
+	if(getwindow(display, Refnone) < 0)
+		sysfatal("getwindow: %r");
 	redraw();
-	flushimage(display, 1);
 }
 
 int
 scroll(int d)
 {
 	scroffset = d;
-	if(scroffset+visprocs>nprocs)
+	if(scroffset+visprocs > nprocs)
 		scroffset = nprocs-visprocs;
 	else if(scroffset<0)
 		scroffset = 0;
@@ -193,8 +190,8 @@ timerproc(void *c)
 	}
 }
 
-int
-drawprocfield(Image* screen, int px, int py, char* fmt, ...)
+void
+drawprocfield(int *px, int py, char *fmt, ...)
 {
 	char buf[1024];
 	int n;
@@ -205,20 +202,9 @@ drawprocfield(Image* screen, int px, int py, char* fmt, ...)
 	vseprint(buf+n, buf+sizeof(buf), fmt, ap);
 	va_end(ap);
 
-	string(screen, Pt(screen->r.min.x+Scrollwidth+Thoffset+px, screen->r.min.y+Vstep+py+5),
+	string(screen, Pt(screen->r.min.x+Scrollwidth+Thoffset+*px, screen->r.min.y+Vstep+py+5),
 		viewfg, ZP, display->defaultfont, buf);
-	return hstep;
-}
-
-void
-drawproc(int py)
-{
-	Image* line = viewbg;
-
-	line->r = Rect(screen->r.min.x+Scrollwidth, screen->r.min.y+Theight+py,
-				screen->r.max.x, screen->r.min.y+Theight+py+Vstep);
-
-	draw(screen, line->r, line, nil, ZP);
+	*px += hstep;
 }
 
 void
@@ -231,11 +217,11 @@ redraw(void)
 	visprocs = (screen->r.max.y-screen->r.min.y-Thoffset-Vstep)/Vstep;
 	draw(screen, toolr, toolbg, nil, ZP);
 	draw(screen, viewr, viewbg, nil, ZP);
+	yscr = toffset = 0;
 
 	if ((hstep = (screen->r.max.x-screen->r.min.x)/getrowcount()) < Hstep)
 		hstep = Hstep;
 
-	toffset = 0;
 	for(i=0; headers[i]!=nil; i++){
 		if(i==4 && !realtimeflag)
 			continue;
@@ -253,18 +239,18 @@ redraw(void)
 		procx = 0;
 		if(screen->r.min.y+Vstep+procy>screen->r.max.y)
 			break;
-		procx += drawprocfield(screen, procx, procy, "%d", proclist[i].pid);
-		procx += drawprocfield(screen, procx, procy, "%s", proclist[i].user);
-		procx += drawprocfield(screen, procx, procy, "%lud:%.2lud", proclist[i].u/60, proclist[i].u%60);
-		procx += drawprocfield(screen, procx, procy, "%lud:%.2lud", proclist[i].s/60, proclist[i].s%60);
+		drawprocfield(&procx, procy, "%d", proclist[i].pid);
+		drawprocfield(&procx, procy, "%s", proclist[i].user);
+		drawprocfield(&procx, procy, "%lud:%.2lud", proclist[i].u/60, proclist[i].u%60);
+		drawprocfield(&procx, procy, "%lud:%.2lud", proclist[i].s/60, proclist[i].s%60);
 		if(realtimeflag)
-			procx += drawprocfield(screen, procx, procy, "%s", proclist[i].r);
-		procx += drawprocfield(screen, procx, procy, "%dK", proclist[i].siz);
-		procx += drawprocfield(screen, procx, procy, "%s", proclist[i].state);
+			drawprocfield(&procx, procy, "%s", proclist[i].r);
+		drawprocfield(&procx, procy, "%dK", proclist[i].siz);
+		drawprocfield(&procx, procy, "%s", proclist[i].state);
 		if(argumentflag)
-			procx += drawprocfield(screen, procx, procy, "%s", proclist[i].args);
+			drawprocfield(&procx, procy, "%s", proclist[i].args);
 		else
-			procx += drawprocfield(screen, procx, procy, "%s", proclist[i].cmd);
+			drawprocfield(&procx, procy, "%s", proclist[i].cmd);
 		procy += Vstep;
 	}
 
@@ -276,7 +262,7 @@ redraw(void)
 		hscr = ((double)visprocs/nprocs)*Dy(scrollr);
 		yscr = ((double)scroffset/nprocs)*Dy(scrollr);
 		scrposr = Rect(scrollr.min.x, scrollr.min.y+yscr, scrollr.max.x-1, scrollr.min.y+yscr+hscr);
-	}else{
+	} else{
 		scrposr = Rect(scrollr.min.x, scrollr.min.y+yscr, scrollr.max.x-1, scrollr.max.y);
 	}
 	draw(screen, scrollr, scrollbg, nil, ZP);
@@ -295,10 +281,10 @@ loaddir(void)
 	int i, n, ac, fd, statfd, ndirs, pid;
 	long rtime;
 
-	if(proclist!=nil)
+	if(proclist != nil)
 		free(proclist);
 
-	if(chdir("/proc")==-1)
+	if(chdir("/proc") == -1)
 		sysfatal("chdir: %r");
 
 	fd = open(".", OREAD);
@@ -310,12 +296,12 @@ loaddir(void)
 		sysfatal("dirreadall: %r");
 
 	proclist = malloc(ndirs*sizeof(Process));
-	if(proclist==nil)
+	if(proclist == nil)
 		sysfatal("malloc: %r");
 
 	nprocs = 0;
 	for(i=0; i<ndirs; i++) {
-		if(strcmp(dir[i].name,"trace")==0)
+		if(strcmp(dir[i].name,"trace") == 0)
 			continue;
 		pid = atoi(dir[i].name);
 		sprint(buf, "/proc/%d/status", pid);
@@ -323,7 +309,7 @@ loaddir(void)
 		if(statfd<0)
 			continue;
 		n = read(statfd, status, sizeof(status) - 1);
-		if(n<0){
+		if(n < 0){
 			close(statfd);
 			continue;
 		}
@@ -345,9 +331,11 @@ loaddir(void)
 
 		if(realtimeflag){
 			if(rtime >= 86400)
-				sprint(proclist[i].r, " %lud:%02lud:%02lud:%02lud", rtime/86400, (rtime/3600)%24, (rtime/60)%60, rtime%60);
+				sprint(proclist[i].r, " %lud:%02lud:%02lud:%02lud",
+					rtime/86400, (rtime/3600)%24, (rtime/60)%60, rtime%60);
 			else if(rtime >= 3600)
-				sprint(proclist[i].r, " %lud:%02lud:%02lud", rtime/3600, (rtime/60)%60, rtime%60);
+				sprint(proclist[i].r, " %lud:%02lud:%02lud",
+					rtime/3600, (rtime/60)%60, rtime%60);
 			else
 				sprint(proclist[i].r, " %lud:%02lud", rtime/60, rtime%60);
 		}
@@ -355,10 +343,10 @@ loaddir(void)
 		if(argumentflag){
 			sprint(buf, "/proc/%d/args", pid);
 			statfd = open(buf, OREAD);
-			if(statfd<0)
+			if(statfd < 0)
 				continue;
 			n = read(statfd, buf, sizeof(buf)-1);
-			if(n<0)
+			if(n < 0)
 				continue;
 			buf[n] = '\0';
 			strcpy(proclist[nprocs].args, buf);
@@ -374,8 +362,7 @@ loaddir(void)
 void
 loadtheme(void)
 {
-	char* s;
-	char* v[3];
+	char *s, *v[3];
 
 	if((b = Bopen("/dev/theme", OREAD)) != nil){
 		while((s = Brdline(b, '\n')) != nil){
@@ -383,17 +370,23 @@ loadtheme(void)
 			if(!tokenize(s, v, nelem(v)))
 				continue;
 			if(strcmp(v[0], "ltitle") == 0)
-				toolbg = allocimage(display, Rect(0,0,1,1), screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
+				toolbg = allocimage(display, Rect(0,0,1,1),
+					screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
 			if(strcmp(v[0], "menutext") == 0)
-				toolfg = allocimage(display, Rect(0,0,1,1), screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
+				toolfg = allocimage(display, Rect(0,0,1,1),
+					screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
 			if(strcmp(v[0], "back") == 0)
-				viewbg = allocimage(display, Rect(0,0,1,1), screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
+				viewbg = allocimage(display, Rect(0,0,1,1),
+					screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
 			if(strcmp(v[0], "text") == 0)
-				viewfg = allocimage(display, Rect(0,0,1,1), screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
+				viewfg = allocimage(display, Rect(0,0,1,1),
+					screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
 			if(strcmp(v[0], "border") == 0)
-				scrollbg = allocimage(display, Rect(0,0,1,1), screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
+				scrollbg = allocimage(display, Rect(0,0,1,1),
+					screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
 			if(strcmp(v[0], "back") == 0)
-				scrollfg = allocimage(display, Rect(0,0,1,1), screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
+				scrollfg = allocimage(display, Rect(0,0,1,1),
+					screen->chan, 1, strtoul(v[1], nil, 16)<<8 | 0xff);
 		}
 		Bterm(b);
 	} else {
@@ -425,11 +418,9 @@ threadmain(int argc, char *argv[])
 		{ nil, &m,  CHANRCV },
 		{ nil, &k,  CHANRCV },
 		{ nil, nil, CHANRCV },
-		{ nil, nil, CHANRCV },
 		{ nil, nil, CHANEND },
 	};
 
-	scroffset = 0;
 	delay = 5000;
 	argumentflag = 0;
 	reverseoflag = 0;
@@ -441,7 +432,7 @@ threadmain(int argc, char *argv[])
 		argumentflag++;
 		break;
 	case 'd':
-		delay=atoi(EARGF(usage()));
+		delay = atoi(EARGF(usage()));
 		break;
 	case 'h':
 		usage();
@@ -451,7 +442,8 @@ threadmain(int argc, char *argv[])
 	case 'r':
 		realtimeflag++;
 		break;
-	}ARGEND;
+	}
+	ARGEND;
 
 	if(initdraw(nil, nil, argv0) < 0)
 		sysfatal("initdraw: %r");
@@ -467,26 +459,24 @@ threadmain(int argc, char *argv[])
 	proccreate(timerproc, alts[Etimer].c, 1024);
 	loadtheme();
 	loaddir();
-	eresize();
+	goto run;
 
 	for(;;){
 		switch(alt(alts)){
-		case Ekeyboard:
-			ekeyboard(k);
-			break;
 		case Emouse:
 			emouse(&m);
 			break;
+		case Ekeyboard:
+			ekeyboard(k);
+			break;
 		case Eresize:
-			if(getwindow(display, Refnone) < 0)
-				sysfatal("getwindow: %r");
 			eresize();
 			break;
 		case Etimer:
 			loaddir();
 			break;
 		}
+run:
 		redraw();
 	}
-	/* NOT REACHED */
 }
